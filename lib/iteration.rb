26987b2bee64b04a8af59d121f55631a2090c2f9
Benjamin Kagan <kagan.benjamin@gmail.com>
2019-11-22 18:15:24 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -3,7 +3,7 @@ def join_ingredients(src)
  new_array = []  
  row_index = 0 
  while row_index < src.count do 
-   inner =  
+   inner = src[row_index]
    while element_index < src[row_index].count do 
      new_array << "I love #{src[row_index][0]} and #{src[row_index][1]} on my pizza" 
    end 
